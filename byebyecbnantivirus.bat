99e2052b42390e9fc5a2005a11258b0b1e8b8922
nelsonchung <chihchun.chung@gmail.com>
2016-03-23 15:20:38 +0800

    kill cbn antivirus

---
+++ byebyecbnantivirus.bat
@@ -0,0 +1,16 @@
+#netsh interface set interface name="CM" admin=DISABLED
+#netsh interface set interface name="office" admin=DISABLED
+#netsh interface set interface name="lab" admin=DISABLED
+#netsh set interface IfName="lab" admin=DISABLED
+#netsh interface set interface name="lab" admin=disabled
+#netsh 
+#interface
+#set interface name="lab" admin=disabled
+
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\winrdlv3.exe" /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\winrdgv3.exe" /f
+#REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\winrdgv3.exe"
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\winrdgv3.exe" /v Debugger /t REG_SZ /d calc
+reg delete "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /f
+shutdown -r -f -t 0
+
