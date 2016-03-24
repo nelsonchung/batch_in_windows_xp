b4534e5b64ea21ef97085cf400f5b85dc4d317a4
nelsonchung <chihchun.chung@gmail.com>
2016-03-24 11:35:47 +0800

    Modify byebyecbnantivirus.bat and try to fix the problem that the calc process is launched repeatly.

--- byebyecbnantivirus.bat
+++ byebyecbnantivirus.bat
@@ -7,10 +7,12 @@
 #interface
 #set interface name="lab" admin=disabled
 
+#請手動關掉所有網路卡或是拔掉網路線
 reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\winrdlv3.exe" /f
 reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\winrdgv3.exe" /f
 #REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\winrdgv3.exe"
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\winrdgv3.exe" /v Debugger /t REG_SZ /d calc
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\winrdgv3.exe" /v Debugger /t REG_SZ /d note
+#REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\winrdgv3.exe" /v Debugger /t REG_SZ /d calc
 reg delete "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /f
 shutdown -r -f -t 0
-
+#重開機後，請重新恢復網路功能
