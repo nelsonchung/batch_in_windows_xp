344c301afbe40a43570817545acc9b47ac2cc1a8
nelsonchung <chihchun.chung@gmail.com>
2016-05-19 15:26:32 +0800

    Update recoverycbnantivirus.bat

--- recoverycbnantivirus.bat
+++ recoverycbnantivirus.bat
@@ -1,5 +1,4 @@
-#請手動關掉所有網路卡或是拔掉網路線
+#注意，請不用，請不用，請不用手動關掉所有網路卡或是拔掉網路線
 reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\winrdlv3.exe" /f
 reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\winrdgv3.exe" /f
 shutdown -r -f -t 0
-#重開機後，請重新恢復網路功能
